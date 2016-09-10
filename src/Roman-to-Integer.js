077a428a0fbec3331eac43be77dac62a84e63d51
Neekon <neekon91@gmail.com>
2016-09-10 13:59:15 -0700

    romanInt

--- src/Roman-to-Integer.js
+++ src/Roman-to-Integer.js
@@ -15,3 +15,24 @@ const romes = {'I':1, 'IV':4, 'V':5, 'IX':9, 'X':10,'XL':40,'L':50, 'XC':90, 'C'
   }
   return sum;
 };
+// Both solutions work
+
+const Romes = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000
+};
+
+const romanToInt = RN => {
+  let sol = 0;
+  if(!RN.length) return 0;
+  for (let i = RN.length - 1; i >= 0; i--) {
+    (Romes[RN[i+1]] > Romes[RN[i]]) ?
+      sol -= Romes[RN[i]]:sol += Romes[RN[i]];
+  }
+  return sol  ? sol : "null";
+}
