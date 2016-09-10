54c480422275269232e4818cabdd2391e31eb5a2
Neekon <neekon91@gmail.com>
2016-09-10 14:29:33 -0700

    Subsets

---
+++ src/Subsets.js
@@ -0,0 +1,17 @@
+const subsets = nums => {
+  let output = [];
+  nums.sort((a, b) => a - b);
+  
+  const sub = (output, start, end, array, nums) => {
+    output.push(array);
+    let index = start;
+    while (index <= end) {
+        array.push(nums[index]);
+        sub(output, index + 1, end, array.concat(), nums);
+        array.pop();
+        index++;
+    }
+  };
+  sub(output, 0, nums.length - 1, [], nums);
+  return output;
+};
