d2f779cc17a1507041f75fa88c5a94944cc2938c
Neekon <neekon91@gmail.com>
2016-09-10 19:49:24 -0700

    Longest Valid Parentheses 2

---
+++ src/Longest-Valid-Parentheses.js
@@ -0,0 +1,21 @@
+const longestValidParentheses = s => {
+  let last = -1, stack = [], output = 0;
+
+  s.split('').map( (val, index)=> {
+    if (val === '(') {
+      stack.push(index);
+    } else {
+      if (0 === stack.length) {
+        last = index;
+      } else {
+        stack.pop();
+        if (0 === stack.length) {
+          output = Math.max(index - last, output);
+        } else {
+            output = Math.max(index - stack[stack.length - 1], output);
+        }
+      }
+    }
+  });
+  return output;
+};
