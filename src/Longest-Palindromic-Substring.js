b04c2f132b908eeaed9e8fee15959c690043c200
Neekon <neekon91@gmail.com>
2016-09-10 18:31:18 -0700

    Longest Palindromic Substring

---
+++ src/Longest-Palindromic-Substring.js
@@ -0,0 +1,12 @@
+"use strict"
+
+const longestPalindrome = (s) => {
+  for (let len = s.length; len > 0; len--) {
+    for (let index = 0; index <= s.length - len; index++) {
+      let output = s.substr(index,len);
+      if (output.split('').reverse().join('') === output) {
+        return output;
+      }
+    }
+  }
+}
