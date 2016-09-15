e583c253ccb4ae775b1e5bfd4ef5a69cd437c137
Neekon <neekon91@gmail.com>
2016-09-14 23:38:48 -0700

    two sum done

--- src/two-sum.js
+++ src/two-sum.js
@@ -1,15 +1,13 @@
 const twoSum = (nums, target) => {
-    let obj = {},
-    otherVal,
-    solution = [];
+  let obj = {}, otherVal, solution = [];
 
-    nums.forEach((val, i)=>{
-        otherVal = target - val;
-        if(otherVal in obj){
-            solution.push(obj[otherVal], i);
-        } else {
-            obj[val] = i;
-        }
-    });
-    return solution;
+  nums.map((val, i)=>{
+    otherVal = target - val;
+    if(otherVal in obj){
+      solution.push(obj[otherVal], i);
+    } else {
+      obj[val] = i;
+    }
+  });
+  return solution;
 };
