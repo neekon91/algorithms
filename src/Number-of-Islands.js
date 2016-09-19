1e7cb157a644c50980090c0214de8fb42ed306c4
Neekon <neekon91@gmail.com>
2016-09-18 20:40:29 -0700

    count islands complete

---
+++ src/Number-of-Islands.js
@@ -0,0 +1,17 @@
+const numIslands = grid => {
+  let islands = 0;
+  grid.map((_, i)=> grid[i].map((_, z)=>
+      grid[i][z] === '1' ? convert(grid, i, z, islands++): _));
+  return islands;
+};
+
+const convert = (grid, i, z, islands) => {
+  if(0 <= i &&  i < grid.length && grid[i][z] === '1') {
+    grid[i][z] = '.';
+
+    convert(grid, i - 1, z);
+    convert(grid, i, z - 1);
+    convert(grid, i, z + 1);
+    convert(grid, i + 1, z);
+  }
+};
