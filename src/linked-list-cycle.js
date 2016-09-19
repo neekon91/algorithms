78fd30551465d6d4935b9092d39c5ff4724cff41
Neekon <neekon91@gmail.com>
2016-09-18 19:50:43 -0700

    cycle fix

--- src/linked-list-cycle.js
+++ src/linked-list-cycle.js
@@ -1,8 +1,7 @@
 const hasCycle = head => {
     if(!head) return false;
-    if(head.prev){
-        return head.prev;
-    }else{
+    if(head.prev) return head.prev;
+    else{
         head.prev = true;
         return hasCycle(head.next);
     }
