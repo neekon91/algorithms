ca2b99827ee572c2acdc76f53effc53b881db920
Neekon <neekon91@gmail.com>
2016-11-05 21:47:28 -0700

    insertion sort

---
+++ src/insertion-sort.js
@@ -0,0 +1,12 @@
+const insertionSort = nums => {
+  for(let i = 1; i < nums.length; i++){
+    for(let z = 0; z < i; z++){
+      if(nums[i] < nums[z]){
+        [nums[z], nums[i]] = [nums[i], nums[z]]
+      }
+    }
+  }
+  return nums;
+}
+
+console.log(insertionSort([5,3,6,3,3,1]))
