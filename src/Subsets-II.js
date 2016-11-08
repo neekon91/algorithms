4c2e4510eabc88edbca0c94fad829eff5fc2659e
Neekon <neekon91@gmail.com>
2016-11-07 19:30:22 -0800

    dry code

--- src/Subsets-II.js
+++ src/Subsets-II.js
@@ -1,15 +1,15 @@
 const subsetsWithDup = (nums, output = [], combo = [], obj = {}) =>{
   nums.sort((a, b) => a - b);
-  (dfs = (index, nums) => {
+  (dfs = index => {
     if (!obj[combo.toString()]) {
       obj[combo.toString()] = combo;
       output = [...output, combo.slice()];
     }
     for (let i = index; i < nums.length; i++) {
       combo = [...combo, nums[i]];
-      dfs(i + 1, nums);
+      dfs(i + 1);
       combo.pop();
     }
-  })(0, nums);
+  })(0);
   return output;
 };
