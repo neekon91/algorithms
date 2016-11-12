0a50d01385debb1222411cb77bb7fba9995bb29a
Neekon <neekon91@gmail.com>
2016-11-11 21:06:48 -0800

    max sub

--- src/Maximum-Subarray.js
+++ src/Maximum-Subarray.js
@@ -1,12 +1,9 @@
-const maxSubArray = nums => {
-  let sum = 0,
-  max = -Infinity;
-  nums.forEach(val => {
-      sum += val;
-      max = Math.max(sum, max);
-      if(sum < 0){
-          sum = 0;
-      }
-  });
-  return max;
-};
+const maxSubArray = (nums, sum = 0)=>
+  nums.reduce((max, val) => {
+    if(sum < 0){
+      sum = 0;
+    }
+    sum += val;
+    max = Math.max(sum, max);
+    return max;
+  }, -Infinity);
