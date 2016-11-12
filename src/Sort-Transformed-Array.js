5170293fd1de731f923064bdba4751e880f60137
Neekon <neekon91@gmail.com>
2016-11-11 21:45:08 -0800

    fix bug dry

--- src/Sort-Transformed-Array.js
+++ src/Sort-Transformed-Array.js
@@ -1,6 +1,3 @@
-const sortTransformedArray = (nums, a, b, c) => {
-  let output = [];
-  nums.forEach(val => output.push(a * (val * val) + (b * val) + (c)));
-  output.sort( (a, b)=> a - b );
-  return output;
-};
+const sortTransformedArray = (nums, a, b, c) =>
+  nums.reduce((output, val) => [...output, (a * (val * val) + (b * val) + (c))], [])
+  .sort( (a, b)=> a - b );
