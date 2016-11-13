51ff88195152d5b91d15a4dd722b87f7609ed491
Neekon <neekon91@gmail.com>
2016-11-12 19:59:55 -0800

    refactor combinations

--- src/combinations.js
+++ src/combinations.js
@@ -1,15 +1,14 @@
 const combine = (n, k, result = []) => {
   (sub = (start, combo, k) => {
-      if (combo.length === k) {
-          result.push(combo);
-          return;
-      }
+    if (combo.length === k) {
+      return result.push(combo);
+    }
 
-      for (let i = start; i <= n; i++) {
-          combo.push(i);
-          sub(i + 1, combo.concat(), k);
-          combo.pop();
-      }
+    for (let i = start; i <= n; i++) {
+      combo.push(i);
+      sub(i + 1, combo.concat(), k);
+      combo.pop();
+    }
   })(1, [], k)
 
   return result;
