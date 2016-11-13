aacab47856cac54c72d4f10f536d287b2f1550c2
Neekon <neekon91@gmail.com>
2016-11-13 00:27:37 -0800

    selection sort

---
+++ src/selectionSort.js
@@ -0,0 +1,18 @@
+const nums = [9, 2, 5, 11, 6, 4, 12, 3, 7, 10, 1, 8];
+
+const selectionSort = nums => {
+  nums.forEach((val, i)=>{
+    let min = i;
+    for(let z = i + 1; z < nums.length; z++) {
+      if(nums[z] < nums[min]) {
+        min = z;
+      }
+    }
+    if(i !== min) {
+      [nums[i], nums[min]] = [nums[min], nums[i]];
+    }
+  })
+  return nums;
+}
+
+console.log(selectionSort(nums)); // => [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ]
