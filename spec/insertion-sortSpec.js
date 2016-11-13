5d2c5d3dfa2e57f7f5c40810817b4764f24787b4
Neekon <neekon91@gmail.com>
2016-11-13 13:32:57 -0800

    merge sort tests

--- spec/insertion-sortSpec.js
+++ spec/insertion-sortSpec.js
@@ -7,6 +7,19 @@ describe('insertionSort', function() {
 
   });
 
+  it('should not use Array.sort', function(){
+    var _sort = Array.prototype.sort;
+    Object.defineProperty(Array.prototype, 'sort', {enumerable: false,
+      value: function () {
+        should.fail(null, null, 'Array.sort called! That\'s cheating.',null);
+      }
+    });
+    var result = insertionSort([8,7,3,6,9,2,4,5,1]);
+    Object.defineProperty(Array.prototype, 'sort', {enumerable: false,
+      value: _sort
+    });
+  });
+  
   it('should sort an unsorted array of numbers', function(){
     sort = insertionSort([10,4,3,2,5,6,7,8,1,9]);
     expect(sort).to.eql([ 1,2,3,4,5,6,7,8,9,10 ]);
--- src/merge-sort.js
+++ src/merge-sort.js
@@ -1,13 +1,8 @@
-const mergeSort = nums =>
-  (nums.length < 2) ? nums :
-  merge(
-    nums.slice(0, Math.floor(nums.length / 2)),
-    nums.slice(Math.floor(nums.length / 2))
-  )
-
-const merge = (left , right, output = []) => {
-  while(left.length && right.length){
-    output.push(right[0] > left[0] ? left.shift(): rigth.shift())
-  }
-  return output;
+let mergeSort = arr => (arr.length < 2) ? arr :
+  merge(mergeSort(arr.slice(0,Math.floor(arr.length /2))),
+        mergeSort(arr.slice(Math.floor(arr.length /2))));
+let merge = (l,r, solution = [])=> {
+  while (l.length && r.length)
+    solution.push(r[0] > l[0] ? l.shift() : r.shift());
+  return solution.concat(l.length? l : r);
 }
