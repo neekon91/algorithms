c52e9cae8d2084d7c139af7142c335e1bcb2f041
Neekon <neekon91@gmail.com>
2016-11-13 17:53:45 -0800

    cycles test complete

--- src/linked-list-cycle.js
+++ src/linked-list-cycle.js
@@ -1,3 +1,27 @@
+const hasCycle = (linkedList, double) => {
+  double = double ? double : linkedList.next;
+
+  if (linkedList === double){
+    return true;
+  } else if (linkedList.next === null || double.next === null){
+    return false;
+  } else {
+    return hasCycle(linkedList.next, double.next.next);
+  }
+
+};
+
+const hasCycle = (linkedList) => {
+  let gas = true, fast = linkedList;
+  let slow = linkedList;
+  while (fast = fast.next) {
+    if (fast === slow) return true;
+    slow = gas ? slow : slow.next;
+    gas = !gas;
+  }
+  return false;
+};
+
 const hasCycle = head => {
     if(!head) return false;
     if(head.prev) return head.prev;
