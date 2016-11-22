0584feb163895c51e7551def7d16b9d3cedf2c9f
Neekon <neekon91@gmail.com>
2016-11-22 12:40:01 -0800

    bug fix

--- src/Spiral-Matrix.js
+++ src/Spiral-Matrix.js
@@ -1,11 +1,9 @@
 const spiralOrder = (matrix, output = []) => {
 
   if(matrix.length === 0 || matrix === null || matrix[0].length === 0) {
-  //filter undefined
-    return output
-      .reduce((arr, elem) => elem !== undefined ? [...arr, elem]: arr, []);
+    return output.reduce((arr, elem) => elem !== undefined ? [...arr, elem]: arr, []);
   }
-  //add top row || get elements on the right
+
   output = output.concat(matrix.shift())
     .concat(matrix.reduce((arr, rightElements)=>
     arr = [...arr, rightElements.pop()], []));
@@ -13,7 +11,7 @@ const spiralOrder = (matrix, output = []) => {
   if (matrix.length === 0) {
     return output;
   }
-  //get the bottom row || reverse it || get the elements on the left
+  
   output = output.concat(matrix.pop().reverse())
     .concat(matrix.reduceRight((arr, leftElements)=>
       arr = [...arr, leftElements.shift()], []));
