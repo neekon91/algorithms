a9f94cfd16d27a14f506a92d59002af2a77672ed
Neekon <neekon91@gmail.com>
2017-01-26 13:32:26 -0800

    roman update

--- src/Roman-to-Integer.js
+++ src/Roman-to-Integer.js
@@ -1,20 +1,7 @@
-const romanToInt = s => {
 const romes = {'I':1, 'IV':4, 'V':5, 'IX':9, 'X':10,'XL':40,'L':50, 'XC':90, 'C':100, 'CD':400, 'D':500, 'CM':900, 'M':1000};
-  let arr = s.split(''),
-  sum = 0,
-  temp;
-  while(arr.length > 0){
-    temp = arr[0].concat(arr[1]);
-    if(romes[temp]){
-      sum += romes[temp];
-      arr = arr.slice(2);
-    } else {
-      sum += romes[arr[0]];
-      arr = arr.slice(1);
-    }
-  }
-  return sum;
-};
+const romanToInt = s =>
+  [...s].reduce((memo, val, i)=>
+    memo += (romes[s[i+1]] > romes[val]) ? -romes[val]: +romes[val], 0);
 
 // solution 2
 // ===================
