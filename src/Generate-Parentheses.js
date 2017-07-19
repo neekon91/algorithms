45c234cad7cc8adbecd85aa1922f8121ae74197d
Neekon Etemad <neekon91@gmail.com>
2017-07-19 14:56:24 -0700

    Update Generate-Parentheses.js

--- src/Generate-Parentheses.js
+++ src/Generate-Parentheses.js
@@ -1,14 +1,10 @@
 const generateParenthesis = (n, output = []) => {
   (dfs = (combo, open, close) => {
-  	if (open + close === n * 2) {
-      return output = [...output, combo];
-  	}
-  	if (close < n && close < open)
-  	  dfs(`${combo})`, open, close + 1);
-
-  	if (open < n)
-    	dfs(`${combo}(`, open + 1, close);
-
-	})('', 0, 0);
+    if (open + close === n * 2) return output = [...output, combo];
+	  
+    if (close < n && close < open) dfs(`${combo})`, open, close + 1);
+	  
+    if (open < n) dfs(`${combo}(`, open + 1, close);  
+  })('', 0, 0);
   return output;
 };
